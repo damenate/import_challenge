10b55e3b6f685ebc84b0d0e9c4d0bb2e3936ff83
Da-Me <damekim1206@gmail.com>
2016-05-05 09:57:35 -0400

    add validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+has_secure_password
+
+validates :name, presence: {message: "First name must be present"}
+validates :email, presence: {message: "Email must be present"}, uniqueness: true
+validates :password, length: { minimum: 8, message: "Password must be at least 8 characters" },
 end
