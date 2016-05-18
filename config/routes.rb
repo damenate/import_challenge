e5c5e2432dd1502a5f534fcabb714c5f90a1e155
Da-Me <damekim1206@gmail.com>
2016-05-18 17:40:25 -0400

    add some devise

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'sessions/create'
 
   get 'sessions/destroy'
+  root :to => "home#index"
 
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
